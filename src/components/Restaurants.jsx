8aa8bc522c12ac47650b1b3c1ff149a77459f215
Tyson Unce <Tyson@MacBook-Pro.attlocal.net>
2022-09-14 21:40:50 -0500

    fingies crossed

--- src/components/Restaurants.jsx
+++ src/components/Restaurants.jsx
@@ -3,19 +3,16 @@ import './compStyles/Restaurants.css';
 import { useEffect, useState } from 'react';
 import Axios from 'axios';
 const Restaurants = () => {
-	const [restaurants, showRestaurants] = useState([]);
-	const [searchTerm, setSearchTerm] = useState('');
+	const [products, showProducts] = useState([]);
+
+	//usEffect tells it to do something on render, in this case I'm rendering the API when my component loads
 	useEffect(() => {
-		Axios.get(
-			'https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+near+me&key=AIzaSyBb_zO6I2OtyHIfO9vUazpYzo37wPLrKTY'
-		).then((response) => {
-			showRestaurants(response.data);
-		});
+		Axios.get('https://smartecombackend.herokuapp.com/getProducts').then(
+			(response) => {
+				showProducts(response.data);
+			}
+		);
 	}, []);
-	let rawData = restaurants.results;
-	// for (let i = 0; i < rawData.length; i++) {
-	// 	console.log(rawData[i]['business_status']);
-	// }
 	return (
 		<div id='restPage'>
 			<h1 className='restHeading'>Near You</h1>
@@ -23,27 +20,31 @@ const Restaurants = () => {
 				<iframe src='https://www.google.com/maps/embed/v1/search?q=restaurants%20near%20me&key=AIzaSyBb_zO6I2OtyHIfO9vUazpYzo37wPLrKTY'></iframe>
 
 				<div className='restRest'>
-					<div className='divScrolly'>
-						<div className='restInfo'>
-							<h1>El Rincon</h1>
-							<div className='restGrid'>
-								<div className='restLoc'>
-									<h3>this is the address</h3>
-									<p>their website</p>
-								</div>
-								<div>
-									<button
-										className='historyButton'
-										onClick={() => {
-											console.log('hey');
-										}}
-									>
-										check-in
-									</button>
+					{products.map((val) => {
+						return (
+							<div className='divScrolly'>
+								<div className='restInfo'>
+									<h1>{val.name}</h1>
+									<div className='restGrid'>
+										<div className='restLoc'>
+											<h3>{val.address}</h3>
+											<p>{val.website}</p>
+										</div>
+										<div>
+											<button
+												className='historyButton'
+												onClick={() => {
+													console.log('hey');
+												}}
+											>
+												check-in
+											</button>
+										</div>
+									</div>
 								</div>
 							</div>
-						</div>
-					</div>
+						);
+					})}
 				</div>
 			</div>
 		</div>
